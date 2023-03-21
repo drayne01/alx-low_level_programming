c955e6614d4e91806942ef8e3195bc639822e5ae
Dominic Akamara Alokopo <dalokopo@gmail.com>
2023-03-21 04:47:15 -0700

    7-print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Main - prints the lowercase alphabet in reverse.
+ * main - prints the lowercase alphabet in reverse.
  * followed by a new line
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
