7bdc94083b9ef6bd4777fdd4317c330417a99cbf
Dominic Akamara Alokopo <dalokopo@gmail.com>
2023-03-20 19:07:31 -0700

    C Variables if else while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Main - prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
