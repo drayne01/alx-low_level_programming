540fb366267281188a64cf573f0523abfbd8acaf
Dominic Akamara Alokopo <dalokopo@gmail.com>
2023-03-21 02:20:03 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * Main -  Prints numbers from 0 to 9
- *
+ * main - prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
