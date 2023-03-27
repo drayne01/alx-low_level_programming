1ef8724f0c2a13259eea705ec6b86949b509202c
Dominic Akamara Alokopo <dalokopo@gmail.com>
2023-03-27 03:45:34 -0700

    updated

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,16 @@
 
 void print_line(int n)
 {
-	int i
+	int i;
 
 	if (n <= 0)
 	{
 		_putchar('\n');
-	} else
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar(95);
-		}
-		_putchar('\n');
+		return;
 	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
