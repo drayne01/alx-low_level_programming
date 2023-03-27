147918050de4e4e5823888e2ba83a636b51b458d
Dominic Akamara Alokopo <dalokopo@gmail.com>
2023-03-27 03:02:32 -0700

    updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,21 +2,21 @@
 
 /**
  * print_most_numbers - checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
+ * Description: Prints the numbers excluding 2 and 4
+ * Return: The numbers sinc 0 up to 9
  */
 void print_most_numbers(void)
 {
-	int c;
+	int c = 0;
 
-	for (c = 48; c < 58; c++)
+	for (; c <= 9; c++)
 	{
-		if (c != 50)
+		if (c == 2 || c == 4)
 		{
-			if (c != 52)
-			{
-				_putchar(c);
-			}
+			continue;
+		} else
+		{
+			_putchar(c + '0');
 		}
 	}
 	_putchar('\n')
