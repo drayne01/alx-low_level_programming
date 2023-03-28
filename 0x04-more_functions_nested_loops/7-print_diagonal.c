f6d7d29bdadf40f2d2602bd1e2e36e98b4e051c2
Dominic Akamara Alokopo <dalokopo@gmail.com>
2023-03-28 02:28:43 -0700

    updated

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,27 +5,22 @@
  * @n: number of times the character \ should be printed
  */
 
-void print_diagnal(int n)
+void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
-		int i, j;
-		
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < i; j++)
 			{
-				if (j == i)
-				{
-					_putchar('\\');
-				} else if (j < i)
-				{
-					_putchar(' ');
-				}
+				_putchar(32);
 			}
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
