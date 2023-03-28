e8524539429792676281a8f4cfac821b202919c7
Dominic Akamara Alokopo <dalokopo@gmail.com>
2023-03-28 02:41:39 -0700

    updated

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,18 +6,18 @@
  */
 void print_square(int size)
 {
+	int i, j;
+
 	if (size <= 0)
 	{
-		_putchar('\n')
+		_putchar('\n');
 	} else
 	{
-		int i, j;
-
-		for (i == 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
 			_putchar('\n');
 		}
